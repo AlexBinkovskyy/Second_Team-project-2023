1e1322f1ee438e6f57757876354cd82832fc943b
Futozhka <124589958+Futozhka@users.noreply.github.com>
2023-12-22 02:13:43 +0000

    start

---
+++ src/js/pagination.js
@@ -0,0 +1,75 @@
+const startBtn = document.querySelector("#startBtn"),
+  endBtn = document.querySelector("#endBtn"),
+  prevNext = document.querySelectorAll(".prevNext"),
+  numbers = document.querySelectorAll(".link");
+
+
+let currentStep = 0;
+
+
+const updateBtn = () => {
+
+  if (currentStep === 4) {
+    endBtn.disabled = true;
+    prevNext[1].disabled = true;
+  } else if (currentStep === 0) {
+   
+    startBtn.disabled = true;
+    prevNext[0].disabled = true;
+  } else {
+    endBtn.disabled = false;
+    prevNext[1].disabled = false;
+    startBtn.disabled = false;
+    prevNext[0].disabled = false;
+  }
+};
+
+
+numbers.forEach((number, numIndex) => {
+  number.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    currentStep = numIndex;
+
+    document.querySelector(".active").classList.remove("active");
+
+    number.classList.add("active");
+    updateBtn(); 
+  });
+});
+
+
+prevNext.forEach((button) => {
+  button.addEventListener("click", (e) => {
+   
+    currentStep += e.target.id === "next" ? 1 : -1;
+    numbers.forEach((number, numIndex) => {
+     
+      number.classList.toggle("active", numIndex === currentStep);
+      updateBtn();
+    });
+  });
+});
+
+
+startBtn.addEventListener("click", () => {
+  document.querySelector(".active").classList.remove("active");
+  
+  numbers[0].classList.add("active");
+  currentStep = 0;
+  updateBtn(); 
+  endBtn.disabled = false;
+  prevNext[1].disabled = false;
+});
+
+
+endBtn.addEventListener("click", () => {
+ 
+  document.querySelector(".active").classList.remove("active");
+  
+  numbers[4].classList.add("active");
+  currentStep = 4;
+  updateBtn(); 
+  startBtn.disabled = false;
+  prevNext[0].disabled = false;
+});
