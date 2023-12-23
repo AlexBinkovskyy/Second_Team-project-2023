45ff0ed5afebe1f4a31afc2ea13a799d5a9d36bb
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-12-23 19:38:56 +0200

    product list all width

--- src/js/query.js
+++ src/js/query.js
@@ -10,19 +10,31 @@ let limitPerPage = 6;
 let Id = null;
 
 export async function getProdByParams() {
-  return (await axios(`${references.MAIN_URL}?page=${page}&limit=${limitPerPage}`));
+  if (window.innerWidth >= 1440) {
+    console.log(1440);
+    limitPerPage = 9;
+  } else if (window.innerWidth >= 768) {
+    console.log(768);
+    limitPerPage = 8;
+  } else {
+    console.log(375);
+    limitPerPage = 6;
+  }
+  return await axios(
+    `${references.MAIN_URL}?page=${page}&limit=${limitPerPage}`
+  );
 }
 
 export async function getProdByPopular() {
-  return (await axios(`${references.MAIN_URL}/${references.popular}?limit=${limitPerPage}`));
+  return await axios(
+    `${references.MAIN_URL}/${references.popular}?limit=${limitPerPage}`
+  );
 }
 
 export async function getProdByDiscount() {
-  return (await axios(`${references.MAIN_URL}/${references.discount}`));
+  return await axios(`${references.MAIN_URL}/${references.discount}`);
 }
 
 export async function getProdByID() {
-  return (await axios(`${references.MAIN_URL}/${Id}`));
+  return await axios(`${references.MAIN_URL}/${Id}`);
 }
-
-
