e21b4cba15d1240a933b58089abaed6d3b3f21e2
Sergey <110050958+SergeyGusar@users.noreply.github.com>
2023-12-24 21:35:27 +0200

    add adaptive

--- src/js/popular-product.js
+++ src/js/popular-product.js
@@ -1,5 +1,5 @@
 
-import shoppingSvg from  '../images/icons/shopping-cart.svg';
+import shoppingSvg from  '../images/sprite.svg';
 
 
 const popularProduct = document.querySelector('.popular-product-list');
@@ -13,7 +13,7 @@ function markup(arr) {
     .map(
       ({ _id, name, img, category, size, popularity }) => `
           <li class="popular-product-item" id="${_id}">
-            <img src="${img}" alt="${name}" loading="lazy" class="popular-product-pic" />
+            <img src="${img}" alt="${name}" width="56" height="56" loading="lazy" class="popular-product-pic" />
             <div class="popular-product-info">
             <h3 class="popular-product-name">${name}</h3>
               <div class="info-item-description">
@@ -33,7 +33,8 @@ function markup(arr) {
             </div>
             <div class="popular-btn">
             <button type="button" class="popular-buy buy-btn" onclick="addToCart('${_id}')">
-            <img class="koshik"  src="${shoppingSvg}" alt="shop-icon" width="12" height="12"/>
+            <svg class="img-icon"  width="12" height="12">
+            <use href="${shoppingSvg}#icon-shopping-cart"></use></svg>
             </button>
             </div>
           </li>
