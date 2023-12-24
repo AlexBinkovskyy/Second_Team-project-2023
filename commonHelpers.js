ef126446013ed353081b06b20bbf6f4979c820ec
AlexBinkovskyy <138527754+AlexBinkovskyy@users.noreply.github.com>
2023-12-24 17:27:09 +0000

    Deploying to gh-pages from @ AlexBinkovskyy/Second_Team-project-2023@a3f5240068a0d0e042ac14e2e2eb09d8d0fb7185 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,27 +1,27 @@
-import{g as i}from"./assets/main-23ed4d74.js";import"./assets/vendor-f9e4276b.js";const l=document.querySelector(".js-cart-list"),u=[{email:"",products:[{id:"640c2dd963a319ea671e383b",amount:1},{id:"640c2dd963a319ea671e3864",amount:1}]}];p(u[0].products);function p(t){m(t,v).then(a=>{l.insertAdjacentHTML("beforeend",a)})}async function m(t,a){return(await f(t)).map(s=>a(s)).join("")}async function f(t){return await Promise.all(t.map(({id:a})=>i(a).then(e=>e.data)))}function v(t){const{_id:a,name:e,img:s,category:c,price:n,size:r}=t,d=c.replace(/_/g," ");return`
-    <li class="selectedProduct" data-id=${a}>
+import{g as d}from"./assets/main-8a245b4d.js";import"./assets/vendor-f9e4276b.js";const l="/Second_Team-project-2023/assets/sprite-67d7c706.svg",u=document.querySelector(".js-cart-list"),p=[{email:"",products:[{id:"640c2dd963a319ea671e383b",amount:1},{id:"640c2dd963a319ea671e3864",amount:1}]}];m(p[0].products);function m(t){f(t,g).then(e=>{u.insertAdjacentHTML("beforeend",e)})}async function f(t,e){return(await v(t)).map(a=>e(a)).join("")}async function v(t){return await Promise.all(t.map(({id:e})=>d(e).then(s=>s.data)))}function g(t){const{_id:e,name:s,img:a,category:o,price:n,size:r}=t,i=o.replace(/_/g," ");return`
+    <li class="selectedProduct" data-id=${e}>
       <div class="product-picture">
-          <img src="${s}" alt="${e}" class="" loading="lazy" />
+          <img src="${a}" alt="${s}" class="" loading="lazy" />
       </div>
 
       <div class="product-info-container">
 
       <div class="info-header">
-        <h2 class="product-name">${e}</h2>
+        <h2 class="product-name">${s}</h2>
         <button class="delete-btn">
           <svg class="" width="20" height="20">
-            <use href=""></use>
+            <use href="${l}#icon-Cross_close"></use>
           </svg>
         </button>
       </div>
 
       <div class="product-info">
-        <p class="info-quality-category"> Category: <span class="info-value">${d}</span></p>
+        <p class="info-quality-category"> Category: <span class="info-value">${i}</span></p>
         <p class="info-quality"> Size:<span class="info-value">${r}</span></p>
       </div>
         <div class="price">$
           <span>${n}</span>
         </div>
       </div>
-  </li>`}const g=document.querySelector(".cart-form"),y=document.querySelector(".js-modal-close"),o=document.querySelector(".cart-modal-bg");console.log("Modal js");g.addEventListener("submit",h);y.addEventListener("click",C);function h(t){t.preventDefault(),o.classList.remove("hiden")}function C(){o.classList.add("hiden")}
+  </li>`}const y=document.querySelector(".cart-form"),h=document.querySelector(".js-modal-close"),c=document.querySelector(".cart-modal-bg");console.log("Modal js");y.addEventListener("submit",C);h.addEventListener("click",$);function C(t){t.preventDefault(),c.classList.remove("hiden")}function $(){c.classList.add("hiden")}
 //# sourceMappingURL=commonHelpers.js.map
