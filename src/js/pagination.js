1e1fda321270ade1d159b448fa5549bc6784cb29
Futozhka <124589958+Futozhka@users.noreply.github.com>
2023-12-25 07:09:27 +0000

    pagination3

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,81 +1,118 @@
 import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import axios from 'axios';
+import { getProdByParams } from './query';
+import { renderProductList } from './product-list';
+import references from './references';
 
 
-
-export function pagination() {
-  const startBtn = document.querySelector("#startBtn"),
-  endBtn = document.querySelector("#endBtn"),
-  prevNext = document.querySelectorAll(".prevNext"),
-  numbers = document.querySelectorAll(".link");
-
-
-let currentStep = 0;
-
-
-const updateBtn = () => {
-
-  if (currentStep === 4) {
-    endBtn.disabled = true;
-    prevNext[1].disabled = true;
-  } else if (currentStep === 0) {
-
-    startBtn.disabled = true;
-    prevNext[0].disabled = true;
-  } else {
-    endBtn.disabled = false;
-    prevNext[1].disabled = false;
-    startBtn.disabled = false;
-    prevNext[0].disabled = false;
+export async function pagination() {
+    const productList = document.querySelector('.product-list');
+    const container = document.getElementById('pagination1');
+  
+    let page = 1; 
+  
+    async function onPageChanged(event) {
+      page = event.page; 
+  
+      const limit = event.itemsPerPage;
+  
+      try {
+        const response = await getProdByParams(page, limit);
+        const totalItems = response.data.totalCount;
+  
+        renderProductList(response.data); 
+  
+        const pagination = new Pagination(container, {
+          totalItems: totalItems,
+          itemsPerPage: limit,
+          visiblePages: 5,
+          page: page,
+        });
+  
+        pagination.on('afterMove', onPageChanged);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    }
+  
+    onPageChanged({ page, itemsPerPage: 6 }); 
   }
-};
+
+  
+
+    
+//   const startBtn = document.querySelector("#startBtn"),
+//   endBtn = document.querySelector("#endBtn"),
+//   prevNext = document.querySelectorAll(".prevNext"),
+//   numbers = document.querySelectorAll(".pagination-link");
+// let currentStep = 0;
 
 
-numbers.forEach((number, numIndex) => {
-  number.addEventListener("click", (e) => {
-    e.preventDefault();
+// const updateBtn = () => {
 
-    currentStep = numIndex;
+//   if (currentStep === 4) {
+//     endBtn.disabled = true;
+//     prevNext[1].disabled = true;
+//   } else if (currentStep === 0) {
 
-    document.querySelector(".active").classList.remove("active");
-
-    number.classList.add("active");
-    updateBtn(); 
-  });
-});
-
-
-prevNext.forEach((button) => {
-  button.addEventListener("click", (e) => {
-
-    currentStep += e.target.id === "next" ? 1 : -1;
-    numbers.forEach((number, numIndex) => {
-
-      number.classList.toggle("active", numIndex === currentStep);
-      updateBtn();
-    });
-  });
-});
+//     startBtn.disabled = true;
+//     prevNext[0].disabled = true;
+//   } else {
+//     endBtn.disabled = false;
+//     prevNext[1].disabled = false;
+//     startBtn.disabled = false;
+//     prevNext[0].disabled = false;
+//   }
+// };
 
 
-startBtn.addEventListener("click", () => {
-  document.querySelector(".active").classList.remove("active");
+// numbers.forEach((number, numIndex) => {
+//   number.addEventListener("click", (e) => {
+//     e.preventDefault();
 
-  numbers[0].classList.add("active");
-  currentStep = 0;
-  updateBtn(); 
-  endBtn.disabled = false;
-  prevNext[1].disabled = false;
-});
+//     currentStep = numIndex;
+
+//     document.querySelector(".active").classList.remove("active");
+
+//     number.classList.add("active");
+//     updateBtn(); 
+//   });
+// });
 
 
-endBtn.addEventListener("click", () => {
+// prevNext.forEach((button) => {
+//   button.addEventListener("click", (e) => {
 
-  document.querySelector(".active").classList.remove("active");
+//     currentStep += e.target.id === "next" ? 1 : -1;
+//     numbers.forEach((number, numIndex) => {
 
-  numbers[4].classList.add("active");
-  currentStep = 4;
-  updateBtn(); 
-  startBtn.disabled = false;
-  prevNext[0].disabled = false;
-});
-  }
+//       number.classList.toggle("active", numIndex === currentStep);
+//       updateBtn();
+//     });
+//   });
+// });
+
+
+// startBtn.addEventListener("click", () => {
+//   document.querySelector(".active").classList.remove("active");
+
+//   numbers[0].classList.add("active");
+//   currentStep = 0;
+//   updateBtn(); 
+//   endBtn.disabled = false;
+//   prevNext[1].disabled = false;
+// });
+
+
+// endBtn.addEventListener("click", () => {
+
+//   document.querySelector(".active").classList.remove("active");
+
+//   numbers[4].classList.add("active");
+//   currentStep = 4;
+//   updateBtn(); 
+//   startBtn.disabled = false;
+//   prevNext[0].disabled = false;
+// });
+  
