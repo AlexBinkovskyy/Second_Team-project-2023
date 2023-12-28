0372f97c416b2b1779e5d46a8e1660a10d1a63de
AlexBinkovskyy <138527754+AlexBinkovskyy@users.noreply.github.com>
2023-12-28 22:26:27 +0000

    Deploying to gh-pages from @ AlexBinkovskyy/Second_Team-project-2023@1f5189a494653daade58c5b1cb2d23ff4e89216f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{g as s,r as j,a as E,b as H,u as I,s as M,c as T}from"./assets/workWithCart-5e9a4738.js";import"./assets/vendor-8aa786c2.js";const i=document.querySelector(".js-cart-list"),w=document.querySelector(".cart-form-input"),u=document.querySelector(".js-total-summ"),b=document.querySelectorAll(".js-cart-numbers"),k=document.querySelector(".js-clear-order-btn"),g=document.querySelector(".js-cart-box"),y=document.querySelector(".js-empty-cart");let F=0,o=s(),a=null;B();i.addEventListener("click",t=>{if(t.target.classList.contains("delete-btn")){let e=t.target.closest(".selectedProduct");j(e.dataset.id);const n=o.products.findIndex(r=>r.id===e.dataset.id);console.log("index",n),n!==-1&&(o.products.splice(n,1),a.splice(n,1)),console.log(o),console.log(a),m()}});k.addEventListener("click",()=>{E(),o.products=[],a=[],m()});async function B(){o=s(),u.textContent="$0";const t=o.products.length;if(L(t),t){i.innerHTML="";const e=await z(o.products);a=e,console.log(a),S(b,t),C(e),u.textContent="$"+P(e).toFixed(2)}w.value=o.email}async function m(){o=s(),console.log(o);const t=o.products.length;L(t),t&&(u.textContent="$"+P(a).toFixed(2),i.innerHTML="",C(a)),S(b,t)}function C(t){_(t,A).then(e=>{i.insertAdjacentHTML("beforeend",e)})}async function _(t,e){return t.map(n=>(F+=n.price,e(n))).join("")}async function z(t){return await Promise.all(t.map(({id:e})=>H(e).then(n=>n.data)))}function A(t){const{_id:e,name:n,img:r,category:p,price:d,size:q}=t;let f=null;try{f=o.products[o.products.findIndex(l=>l.id===e)].amount}catch(l){return console.log(l),""}const $=p.replace(/_/g," ");return`
+import{g as a,r as H,a as C,b as M,u as T,s as w,c as k,p as F}from"./assets/workWithCart-5294a70b.js";import"./assets/vendor-8aa786c2.js";const i=document.querySelector(".js-cart-list"),O=document.querySelector(".cart-form-input"),p=document.querySelector(".js-total-summ"),L=document.querySelectorAll(".js-cart-numbers"),A=document.querySelector(".js-clear-order-btn"),h=document.querySelector(".js-cart-box"),y=document.querySelector(".js-empty-cart");let B=0,o=a(),c=null;_();i.addEventListener("click",t=>{if(t.target.classList.contains("delete-btn")){let e=t.target.closest(".selectedProduct");H(e.dataset.id);const n=o.products.findIndex(r=>r.id===e.dataset.id);console.log("index",n),n!==-1&&(o.products.splice(n,1),c.splice(n,1)),console.log(o),console.log(c),d()}});A.addEventListener("click",()=>{C(),o.products=[],c=[],d()});async function _(){o=a(),p.textContent="$0";const t=o.products.length;if(P(t),t){i.innerHTML="";const e=await D(o.products);c=e,console.log(c),x(L,t),S(e),p.textContent="$"+q(e).toFixed(2)}O.value=o.email}async function d(){o=a(),console.log(o);const t=o.products.length;P(t),t&&(p.textContent="$"+q(c).toFixed(2),i.innerHTML="",S(c)),x(L,t)}function S(t){z(t,N).then(e=>{i.insertAdjacentHTML("beforeend",e)})}async function z(t,e){return t.map(n=>(B+=n.price,e(n))).join("")}async function D(t){return await Promise.all(t.map(({id:e})=>M(e).then(n=>n.data)))}function N(t){const{_id:e,name:n,img:r,category:f,price:l,size:E}=t;let g=null;try{g=o.products[o.products.findIndex(u=>u.id===e)].amount}catch(u){return console.log(u),""}const I=f.replace(/_/g," ");return`
     <li class="selectedProduct" data-id=${e}>
       <div class="product-picture">
           <img src="${r}" alt="${n}" class="" loading="lazy" />
@@ -12,27 +12,27 @@ import{g as s,r as j,a as E,b as H,u as I,s as M,c as T}from"./assets/workWithCa
 
           <svg class="" width="11.25" height="11.25">
 
-            <use href="${M}#icon-Cross_close"></use>
+            <use href="${w}#icon-Cross_close"></use>
           </svg>
         </button>
       </div>
 
       <div class="product-info">
-        <p class="info-quality-category"> Category: <span class="info-value">${$}</span></p>
-        <p class="info-quality"> Size:<span class="info-value">${q}</span></p>
+        <p class="info-quality-category"> Category: <span class="info-value">${I}</span></p>
+        <p class="info-quality"> Size:<span class="info-value">${E}</span></p>
       </div>
         <div class="price">$
-          <span>${d}</span>
+          <span>${l}</span>
         </div>
       </div>
       <div class="counter-container">
             <button class="counter-btn" type="button" data-action="decrement">
                 -
             </button>
-            <span class="counter-value data-counter">${f}</span>
+            <span class="counter-value data-counter">${g}</span>
             <button class="counter-btn" type="button" data-action="increment">
                 +
             </button>
         </div>
-  </li>`}let c;i.addEventListener("click",function(t){const e=s();let n="",r="";if(t.target.dataset.action==="increment"||t.target.dataset.action==="decrement")n=t.target.closest(".selectedProduct"),r=o.products.findIndex(d=>d.id===n.dataset.id),c=t.target.closest(".counter-container").querySelector(".counter-value");else return;t.target.dataset.action==="increment"&&(c.innerHTML=++c.innerHTML),t.target.dataset.action==="decrement"&&parseInt(c.innerHTML)>1&&(c.innerHTML=--c.innerHTML),I(e.products[r].id,c.innerHTML),m()});function L(t){O(g,y),v(t?g:y)}function v(...t){t.forEach(e=>{e.classList.remove("hiden")})}function O(...t){t.forEach(e=>{e.classList.add("hiden")})}function S(t,e){t.forEach(n=>{n.textContent=e})}function P(t){return t.reduce((e,n)=>e+n.price*o.products[o.products.findIndex(r=>r.id===n._id)].amount,0)}const h=document.querySelector(".cart-form"),D=document.querySelector(".js-modal-close"),x=document.querySelector(".cart-modal-bg");console.log("Hello");h.addEventListener("submit",t=>{t.preventDefault(),T(h.elements.email.value),s(),N()});D.addEventListener("click",U);function N(){x.classList.remove("hiden")}function U(){x.classList.add("hiden")}
+  </li>`}let s;i.addEventListener("click",function(t){const e=a();let n="",r="";if(t.target.dataset.action==="increment"||t.target.dataset.action==="decrement")n=t.target.closest(".selectedProduct"),r=o.products.findIndex(l=>l.id===n.dataset.id),s=t.target.closest(".counter-container").querySelector(".counter-value");else return;t.target.dataset.action==="increment"&&(s.innerHTML=++s.innerHTML),t.target.dataset.action==="decrement"&&parseInt(s.innerHTML)>1&&(s.innerHTML=--s.innerHTML),T(e.products[r].id,s.innerHTML),d()});function P(t){U(h,y),v(t?h:y)}function v(...t){t.forEach(e=>{e.classList.remove("hiden")})}function U(...t){t.forEach(e=>{e.classList.add("hiden")})}function x(t,e){t.forEach(n=>{n.textContent=e})}function q(t){return t.reduce((e,n)=>e+n.price*o.products[o.products.findIndex(r=>r.id===n._id)].amount,0)}const b=document.querySelector(".cart-form"),W=document.querySelectorAll(".js-modal-close"),j=document.querySelector(".success-modal"),$=document.querySelector(".fail-modal");console.log("Hello");b.addEventListener("submit",t=>{t.preventDefault(),k(b.elements.email.value);const e=a();e.products=e.products.map(({id:n,amount:r})=>({productId:n,amount:r})),F(e).then(n=>{n.status===201?(console.log(n.data.message),m(!0),C(),d()):m(!1)}).catch(n=>{m(!1)})});W.forEach(t=>t.addEventListener("click",G));function m(t){t?j.classList.remove("hiden"):$.classList.remove("hiden")}function G(t){j.classList.add("hiden"),$.classList.add("hiden")}
 //# sourceMappingURL=commonHelpers.js.map
